cae0acf4ee2c92f007d4e326577b609e499a8b5b
davidhunter1 <dhuntz65@gmail.com>
2020-04-11 14:08:13 -0400

    hello

--- db/migrate/20200411054630_create_interventions.rb
+++ db/migrate/20200411054630_create_interventions.rb
@@ -1,19 +1,21 @@
 class CreateInterventions < ActiveRecord::Migration[5.2]
   def change
     create_table :interventions do |t|
-      t.references :author, foreign_key: { to_table: 'employees' }, :null => false
-      t.references :customer_id, foreign_key: { to_table: 'customers' }, :null => false
-      t.references :building_id, foreign_key: { to_table: 'buildings' }, :null => false
-      t.references :battery_id, foreign_key: { to_table: 'batteries' }
-      t.references :column_id, foreign_key: { to_table: 'columns' }
-      t.references :elevator_id, foreign_key: { to_table: 'elevators' }
-      t.references :employee_id, foreign_key: { to_table: 'employees' }
-      t.datetime :start_intervention
-      t.datetime :end_intervention
-      t.string :result, :default => "Incomplete"
+      t.integer :author, null: false
+      t.integer :customer_id, null: false
+      t.integer :building_id, null: false
+      t.integer :battery_id
+      t.integer :column_id
+      t.integer :elevator_id
+      t.integer :employee_id
+      t.timestamp :start_intervention
+      t.timestamp :end_intervention
+      t.string :result
       t.string :report
-      t.string :status, :default => "Pending"
+      t.string :status
+
       t.timestamps
     end
   end
 end
+
