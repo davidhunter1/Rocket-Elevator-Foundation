54bb025ebbc5693c356d1669e0017f76fa1a34d3
davidhunter1 <dhuntz65@gmail.com>
2020-04-11 12:44:02 -0400

    auth issue

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+
+  
+  config.action_controller.allow_forgery_protection = false
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
